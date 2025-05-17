4202f4cc444eff3b2df558df583c13e86ee34e1e
Arshadakali <arshad.ali.soomro@gmail.com>
2025-05-17 18:45:52 +0500

    Remove all calculator-desktop and calculator files as requested

--- calculator-web/models/User.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Hashed password
-});
-
-export default mongoose.models.User || mongoose.model('User', UserSchema);
